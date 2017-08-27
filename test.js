fe315aa81067fd06d50d3ea18b1a047b3c7a3cf9
Giorgio Aquino <gigetto@gmail.com>
2017-08-27 11:34:19 +0200

    Add tests

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
 const test = require("tape");
 const catCreator = require("./modules/cats");
 
-test("Cat name should be funny",t => {
-  t.equal(catCreator.createCat("Zelda").funnyName, "^^Zelda^^", 'Should contain ^^ before and after the name');
+const name = "Zelda";
+
+test("Should return an object", t => {
+  t.equal(typeof catCreator.createCat(name), "object");
+  t.end();
+});
+
+test("Cat name should be funny", t => {
+  t.equal(
+    catCreator.createCat(name).funnyName,
+    `^^${name}^^`,
+    "Should contain ^^ before and after the name"
+  );
+  t.ok(
+    catCreator.createCat(name).funnyName.indexOf(name) === 2,
+    "Should contain the name"
+  );
   t.end();
 });
